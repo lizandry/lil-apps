c29bb0399962b620665b2538bf103ed3d712ef21
liz andry <mxlizandry@gmail.com>
2020-11-20 12:48:08 -0800

    finished with this one

--- phaser-intro/assets/js/scenes/TitleScene.js
+++ phaser-intro/assets/js/scenes/TitleScene.js
@@ -6,7 +6,7 @@ class TitleScene extends Phaser.Scene {
 
   create() {
 
-    this.titleText = this.add.text(this.scale.width / 2, this.scale.height / 2, 'tutorial game', { fontSize: '64px', fill : '#fff' });
+    this.titleText = this.add.text(this.scale.width / 2, this.scale.height / 2, 'phaser intro', { fontSize: '64px', fill : '#fff' });
     this.titleText.setOrigin(0.5);
 
     this.startGameButton = new UiButton(this, this.scale.width / 2, this.scale.height * .65, 'button1', 'button2', 'start', this.startScene.bind(this, 'Game'));
